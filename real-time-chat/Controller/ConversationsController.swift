2568d4c61e7081450849966f682037928a4837a3
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2020-10-05 23:31:13 +0900

    initial commit

---
+++ real-time-chat/Controller/ConversationsController.swift
@@ -0,0 +1,38 @@
+//
+//  ConversationsController.swift
+//  real-time-chat
+//
+//  Created by Ted Hyeong on 05/10/2020.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showProfile() {
+        print(123)
+    }
+    
+    //MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+}
---
+++ real-time-chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  real-time-chat
+//
+//  Created by Ted Hyeong on 05/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
